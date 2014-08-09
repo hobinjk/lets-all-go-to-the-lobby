ed2d3bd1ea8b47e3a0519bca91884c5906b64442
James Hobin <hobinjk@mit.edu>
2014-08-09 16:28:49 -0700

    Add config.js

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
 /* jshint node: true */
 
+var config = require('./config') || {};
 var express = require('express');
 var cors = require('cors');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 
-var host = 'mongo.grids.me';
-var port = 27017;
+var dbHost = config.dbHost || 'mongo.grids.me';
+var dbPort = config.dbPort || '27017';
+var dbName = config.dbName || 'sfethics';
+var dbUsername = config.dbUsername;
+var dbPassword = config.dbPassword;
 
-var lobbyContact = []; // this is bad and I should feel bad
-var lobbyMoney = []; // this is also bad and I should continue feeling bad
+var collectionData = {}; // this is somewhat terrible
+var collections = ['lobbycontact', 'lobbymoney', 'filer460'];
 
-console.log('Connecting to ' + host + ':' + port);
+console.log('Connecting to ' + dbHost + ':' + dbPort);
 
-MongoClient.connect('mongodb://mongo.grids.me:27017/sfethics?w=1',
+MongoClient.connect('mongodb://' + dbHost + ':' + dbPort + '/' + dbName +
+    '?w=1',
 function(err, db) {
   if (err) {
     console.log('ERROR: ' + err);
     return;
   }
-  console.log('db?: ' + db);
-  db.admin().authenticate('CodeGuest', '9&Howard', function(err, res) {
-    console.log('err: ' + err);
-    console.log('res: ' + res);
-    db.collection('lobbycontact').find().toArray(function(err, array) {
-        lobbyContact = array;
-    });
-    db.collection('lobbymoney').find().toArray(function(err, array) {
-        lobbyMoney = array;
-    });
+  db.admin().authenticate(dbUsername, dbPassword, function(err, res) {
+    if (!res) {
+      console.log('Authentication error: ' + err);
+      return;
+    }
+
+    function collect(collectionName) {
+      console.log('Getting array for ' + collectionName);
+      db.collection(collectionName).find().toArray(function(err, array) {
+        console.log('Got array for ' + collectionName);
+        collectionData[collectionName] = array;
+      });
+    }
+
+    collections.forEach(collect);
   });
 });
 
@@ -36,12 +46,10 @@ app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 // ROUTES
-app.get('/lobbymoney', function(req, res) {
-  res.send(JSON.stringify(lobbyMoney));
-});
-
-app.get('/lobbycontact', function(req, res) {
-  res.send(JSON.stringify(lobbyContact));
+collections.forEach(function(collectionName) {
+  app.get('/' + collectionName, function(req, res) {
+    res.send(JSON.stringify(collectionData[collectionName]));
+  });
 });
 
 app.listen(3000);
